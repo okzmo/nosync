437664e1a8d4cab76aad8cd2ca9be1a58acb4114
Mathieu Rossi <mathieu-rossi.pro@proton.me>
2024-12-28 01:01:16 +0100

    fix: close menu on click backdrop

--- apps/web/src/ui/layout/backdrop.svelte
+++ apps/web/src/ui/layout/backdrop.svelte
@@ -1,11 +1,15 @@
 <script lang="ts">
 	import { backdrop } from '$lib/stores/backdrop.svelte';
+	import { menu } from '$lib/stores/menu.svelte';
 </script>
 
 <div
 	role="presentation"
 	class="backdrop-transition pointer-events-none fixed left-0 top-0 z-[997] h-screen w-screen bg-black/65 opacity-0 backdrop-blur-md transition-opacity duration-100"
-	onclick={() => backdrop.close()}
+	onclick={() => {
+		menu.closeMenu();
+		backdrop.close();
+	}}
 	class:opacity-100={backdrop.blur}
 	class:pointer-events-auto={backdrop.active}
 ></div>
