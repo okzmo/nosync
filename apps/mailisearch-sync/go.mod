251a9328914256e2ed5fe550b6221da43727238e
Mathieu Rossi <mathieu-rossi.pro@proton.me>
2025-01-03 14:37:26 +0100

    feat: maili service

---
+++ apps/mailisearch-sync/go.mod
@@ -0,0 +1,13 @@
+module github.com/okzmo/mailisearch-sync
+
+go 1.23.4
+
+require github.com/lib/pq v1.10.9
+
+require (
+	github.com/andybalholm/brotli v1.1.1 // indirect
+	github.com/golang-jwt/jwt/v4 v4.5.1 // indirect
+	github.com/josharian/intern v1.0.0 // indirect
+	github.com/mailru/easyjson v0.9.0 // indirect
+	github.com/meilisearch/meilisearch-go v0.30.0 // indirect
+)
---
+++ apps/mailisearch-sync/main.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"database/sql"
+	"encoding/json"
+	"fmt"
+	"log"
+	"os"
+	"time"
+
+	"github.com/lib/pq"
+	"github.com/meilisearch/meilisearch-go"
+)
+
+func main() {
+	connStr := os.Getenv("DB_LINK")
+	_, err := sql.Open("postgres", connStr)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	listener := pq.NewListener(connStr, 10*time.Second, time.Minute, nil)
+	err = listener.Listen("cell_changes")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = listener.Listen("cell_deletion")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	client := meilisearch.New("http://localhost:7700", meilisearch.WithAPIKey(os.Getenv("MAILI_MASTER_KEY")))
+
+	log.Println("Listening to db on cell_changes and cell_deletion!")
+
+	for {
+		notif := <-listener.Notify
+
+		var payload map[string]any
+		if err := json.Unmarshal([]byte(notif.Extra), &payload); err != nil {
+			log.Println("ERROR UNMARSHAL CHANGES:", err)
+		}
+
+		switch notif.Channel {
+		case "cell_deletion":
+			if data, ok := payload["data"].(map[string]any); ok {
+				if id, ok := data["id"].(float64); ok {
+					client.Index("cells").DeleteDocument(fmt.Sprintf("%d", int(id)))
+				}
+			}
+		case "cell_changes":
+			client.Index("cells").AddDocuments(payload["data"])
+		}
+
+	}
+}
